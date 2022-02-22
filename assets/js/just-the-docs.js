09f9024ba647274dec6dc3e088e23c1811fd5c0d
Łukasz Pawela <lukasz.pawela@gmail.com>
2022-02-22 23:06:56 +0100

    update built site

--- assets/js/just-the-docs.js
+++ assets/js/just-the-docs.js
@@ -59,7 +59,7 @@ jtd.getTheme = function() {
 
 jtd.setTheme = function(theme) {
   var cssFile = document.querySelector('[rel="stylesheet"]');
-  cssFile.setAttribute('href', 'https://numericalshadow.org/pages/numericalshadow/numericalshadow.org/assets/css/just-the-docs-' + theme + '.css');
+  cssFile.setAttribute('href', 'http://0.0.0.0:4000/shadow/assets/css/just-the-docs-' + theme + '.css');
 }
 
 // Document ready
